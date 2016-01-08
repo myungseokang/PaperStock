b2c64a64ca56d24fa948ff7c3b5e4c08f71a2f40
myungseo <l3opold7@gmail.com>
2016-01-08 08:09:21 +0000

    이것저것 CSS 사용해보기

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
-@app.route("/hello/<string:to>")
-def helloTo(to="NoBody"):
-    return render_template("index.html", to=to)
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
-@app.route("/world", methods=["GET", "POST"])
-def world():
-    return "world %s" % (request.method,)
+@app.route("/test")
+def test():
+    return render_template("test.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
