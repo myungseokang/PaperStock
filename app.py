cce347fe25acc8bfc2065b0d96d66ca876137de8
myungseoKang <l3opold7@gmail.com>
2016-01-08 16:02:57 +0900

    Set starter-template

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
 
 @app.route("/hello/<string:to>")
 def helloTo(to="NoBody"):
